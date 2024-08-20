f07cfa819b4044f799cad9f870da068fa1c4d2c3
XD8163 <148742955+XD8163@users.noreply.github.com>
2024-08-20 21:47:01 +0800

    Update main.go

--- repository/main.go
+++ repository/main.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"golang-crud-api/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	routes.RegisterBookRoutes(router)
+
+	router.Run(":8080")
+}
